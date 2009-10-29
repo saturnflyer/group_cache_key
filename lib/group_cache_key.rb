543adc13f4489079584b6abe56c6caaafa5cb73a
Jim Gay <jim@saturnflyer.com>
2009-10-29 18:09:48 -0400

    hash the ids in the order given and add to the key

--- lib/group_cache_key.rb
+++ lib/group_cache_key.rb
@@ -1,5 +1,6 @@
 require 'activerecord'
 require 'activesupport'
+require 'digest/md5'
 
 ActiveRecord::Base.class_eval {
   Array.class_eval {
@@ -7,9 +8,10 @@ ActiveRecord::Base.class_eval {
       if self.empty?
         'empty'
       else
+        ids_hash = Digest::MD5.hexdigest(self.collect{|item| item.id }.to_s)
         update_timestamp = max {|a,b| a.updated_at <=> b.updated_at }.updated_at.to_i.to_s
         create_timestamp = max {|a,b| a.created_at <=> b.created_at }.created_at.to_i.to_s
-        self.first.class.to_s.underscore+'/'+length.to_s+'-'+create_timestamp+'-'+update_timestamp
+        self.first.class.to_s.tableize+'/'+length.to_s+'-'+ids_hash+'-'+create_timestamp+'-'+update_timestamp
       end
     end
   }
